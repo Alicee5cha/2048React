663c521cf9404dfef55abcc709e9a74d001cd9f7
Alice Chambers <alice.chambers@autodesk.com>
2020-07-07 11:24:20 +0100

    starting

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+const myfirstelement = <h1>Hello React! index.js stuff</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('main'));
